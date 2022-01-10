00580746c854bc9710435dd11a02b986252d82cc
伊東和成 <itokazunari5375@icloud.com>
2022-01-10 12:39:42 +0900

    インデックス検索を格納

--- script.js
+++ script.js
@@ -15,7 +15,23 @@ class SinglyLinkedList {
       currentNode = currentNode.next;
     }
   }
+  at(index) {
+    let iterator = this.head;
+    // 与えられたindexまでのループをする
+    // nullの場合は末端なので範囲外
+
+    for (let i = 1; i < index; i++) {
+      iterator = iterator.next;
+      if (iterator.next === null) return null;
+    }
+    return iterator;
+  }
 }
+
+const numList = new SinglyLinkedList([
+  35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555,
+]);
+console.log(numList.at(2));
 // class SinglyLinkedList {
 //   constructor(node) {
 //     // 初期値
