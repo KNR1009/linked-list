57b2af61407153c3018729f159afa093fd5547c5
伊東和成 <itokazunari5375@icloud.com>
2022-01-12 08:40:28 +0900

    双方向リストの定義

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 class Node {
-  // 前後を追跡します。
   constructor(data) {
-    this.data = data;
     this.prev = null;
     this.next = null;
+    this.data = data;
   }
 }
 
-// リストは少なくとも1つのノードを持っている必要があります。
-// ヌルリストをサポートしたい場合は、それに応じてコードを追加してください。
 class DoublyLinkedList {
   constructor(arr) {
-    // 今回は末尾を追跡します。
-    if (arr.length <= 0) {
-      this.head = new Node(null);
-      this.tail = this.head;
-      return;
-    }
-
     this.head = new Node(arr[0]);
+
     let currentNode = this.head;
     for (let i = 1; i < arr.length; i++) {
       currentNode.next = new Node(arr[i]);
@@ -27,43 +18,18 @@ class DoublyLinkedList {
       currentNode = currentNode.next;
     }
 
-    // このcurrent Nodeは最後のnodeです。
     this.tail = currentNode;
   }
 
   printList() {
     let iterator = this.head;
     let str = "";
-    while (iterator != null) {
+    while (iterator !== null) {
       str += iterator.data + " ";
       iterator = iterator.next;
     }
     console.log(str);
   }
-
-  at(index) {
-    let iterator = this.head;
-    for (let i = 0; i < index; i++) {
-      iterator = iterator.next;
-      if (iterator === null) return null;
-    }
-    return iterator;
-  }
-
-  // 反対から出力する
-  printInReserve() {
-    let iterator = this.tail;
-    let str = "";
-    while (iterator !== null) {
-      str += iterator.data + " ";
-      iterator = iterator.prev;
-    }
-    return str;
-  }
 }
 
-let numList = new DoublyLinkedList([
-  35, 23, 546, 67, 86, 234, 56, 767, 34, 1, 98, 78, 555,
-]);
-
-console.log(numList.printInReserve());
+const t = new DoublyLinkedList([1, 2, 3, 4]);
